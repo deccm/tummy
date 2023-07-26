b3cd0d8c87cf640f1aa92cb891d96d226b361d89
Dennis Chan <dennisming2012@gmail.com>
2023-07-26 23:32:07 +0800

    chore: add doc

--- src/occupation/index.ts
+++ src/occupation/index.ts
@@ -2,7 +2,7 @@ import { Config } from '../types';
 import { occupations } from './occupations';
 import { sampleSize } from 'lodash';
 
-export function getOccupations(config?: Config) {
+export function getOccupations(config?: Config): string | string[] {
   return !config || config.number === 1
     ? occupations[Math.floor(Math.random() * occupations.length)]
     : sampleSize(occupations, config.number);
