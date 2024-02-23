1abd4068deeb75692010a4de540841f867c75295
Dennis Chan <77636100+den138@users.noreply.github.com>
2024-02-23 22:00:14 +0800

    update get age index.ts

--- src/age/index.ts
+++ src/age/index.ts
@@ -6,15 +6,15 @@ export interface Output {
 }
 
 export function getRandomAge(withDateOfBirth?: boolean): Output {
-  const dOB = getRandomDate(new Date(1920, 0, 1), new Date());
+  const dob = getRandomDate(new Date(1920, 0, 1), new Date());
   const now = new Date();
 
-  const diffInMilliSeconds = now.getTime() - dOB.getTime();
+  const diffInMilliSeconds = now.getTime() - dob.getTime();
   const diffInYears = diffInMilliSeconds / 1000 / 60 / 60 / 24 / 365.25;
   const age = Math.abs(Math.round(diffInYears));
 
   return {
     age,
-    ...(withDateOfBirth && { dateOfBirth: dOB }),
+    ...(withDateOfBirth && { dob }),
   };
 }
